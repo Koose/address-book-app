954851b2aa598554bc0f7535d6b0a993f1fbd222
Linwan Liu <lliu@gorillagroup.com>
2018-02-20 10:43:34 -0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AddressBookComponent } from './address-book/address-book.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddressBookComponent
   ],
   imports: [
     BrowserModule
